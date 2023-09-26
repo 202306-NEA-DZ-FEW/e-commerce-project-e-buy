58591d0d701535f942e406354aba197605573a97
Hadj Said Bouras <hadjsaidbouras@hotmail.com>
2023-09-26 15:42:04 +0100

    feat(componenet): add fetch function
    API #5

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
+import Api from "@/util/API"
+
 export default function Home() {
   return (
     <main>
-      <h1 class="text-center text-5xl font-bold p-20">E-buy coming soon</h1>
+      <Api />
     </main>
   )
 }
