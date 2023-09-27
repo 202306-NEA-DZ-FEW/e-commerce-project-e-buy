2de2cce086fdd94bdac8a6a1aa1fc75ac3db6521
Katia Ghezali <ghezalikatia@hotmail.com>
2023-09-27 19:08:09 +0100

    feat(footer): add a footer with the team members contacts
    Footer #15

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
+import Footer from "@/components/Footer/Footer"
+
 export default function Home() {
   return (
-    <main>
-      <h1 class="text-center text-5xl font-bold p-20">E-buy coming soon</h1>
-    </main>
+    <>
+      <Footer />
+    </>
   )
 }
